cce55d26f3474f49ce771ceb976dd0d2951c0ab7
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2023-05-18 14:06:35 -0300

    hero only vector

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -56,14 +56,14 @@ const Hero = ({ textOne, textTwo, textThree, titleOne, titleTwo, titleThree, img
           <div className="absolute right-[12%]">
             <Image src={heroVector} height="767px" width="768px" className="heroVector" alt='hero-bg-effect' />
           </div>
-          <div className="absolute">
-          <Image src={heladerasLavarropas} height="652px" width="674px" priority className="heroImg" alt='heladera y lavarropas electrolux' />
+          {/* <div className="absolute">
+          <Image src={heladerasLavarropas} height="652px" width="674px" priority className="heroImg" alt='heladera y lavarropas electrolux' /> */}
             {/* { img == 1 ?  <Image src={heladerasLavarropas} height="652px" width="674px" priority className="heroImg" alt='heladera y lavarropas electrolux' />
             // : img == 2 ? <Image src={heladeraHero} height="652px" width="674px" priority className="heroImg" alt='heladera electrolux' />
             // : img == 3 ? <Image src={lavarropasHero} height="652px" width="674px" priority className="heroImg" alt='lavarropas electrolux' />
             // : <Image src={phone} height="652px" width="674px" priority className="heroImg" alt='telefono de contacto' />
             // */}
-          </div>
+          {/* </div> */}
     
           {/* <div className="balls">
           <div className="absolute left-[15%] top-[54%] hidden xl:block">
@@ -107,9 +107,9 @@ const Hero = ({ textOne, textTwo, textThree, titleOne, titleTwo, titleThree, img
         </div> 
         </div> */}
 
-          <div className="absolute md:w-full md:h-full ">
+          {/* <div className="absolute md:w-full md:h-full ">
             <Image src={robotHand} height="652px" width="674px" priority className="robotHand" alt="fondo degradado" />
-          </div>
+          </div> */}
         </div>
       </div>
 
