b8ef113d8d594758173a6d4d47fdb78b32f447a1
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2023-05-19 00:15:14 -0300

    no needed components / button aria-label

--- src/app/components/Counter.js
+++ src/app/components/Counter.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useRef } from "react"
+// import React, { useEffect, useRef } from "react"
 
-function Counter({ from, to }) {
-  const nodeRef = useRef();
+// function Counter({ from, to }) {
+//   const nodeRef = useRef();
 
-  useEffect(() => {
-    const node = nodeRef.current;
+//   useEffect(() => {
+//     const node = nodeRef.current;
 
-    const controls = animate(from, to, {
-      duration: 1,
-      onUpdate(value) {
-        node.textContent = value.toFixed(2);
-      }
-    });
+//     const controls = animate(from, to, {
+//       duration: 1,
+//       onUpdate(value) {
+//         node.textContent = value.toFixed(2);
+//       }
+//     });
 
-    return () => controls.stop();
-  }, [from, to]);
+//     return () => controls.stop();
+//   }, [from, to]);
 
-  return <p ref={nodeRef}  className="text-white text-[29px]"/>;
-}
+//   return <p ref={nodeRef}  className="text-white text-[29px]"/>;
+// }
 
-export default Counter;
+// export default Counter;
--- src/app/components/animations/Button.js
+++ src/app/components/animations/Button.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Button = ({ styles, text, link }) => {
   return(
-    <Link href={`/${link}`}>
+    <Link href={`/${link}`} aria-label={link}>
       <button type="button" className={`py-4 px-6 bg-blue-gradient font-popins font-medium xs:text-[16px] xl:text-[18px] text-primary outline-none rounded-lg ${styles}`}>
         {text}
       </button>
