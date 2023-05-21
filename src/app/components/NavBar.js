591615c702738dbb2acad134b19e0287aa0ac176
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2023-05-21 19:01:08 -0300

    animation nav

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -25,53 +25,29 @@ const NavBar = () => {
           </ul>
           <div className="hidden md:block fixed bottom-4 left-0 w-[100vw] ">
           <a href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20">
-              <Image
-                initial={{ scaleX: 1, sclaeY: 1 }}
-                animate={{ scaleX: 1.1, scaleY: 1.1, transition: { duration: 2, delay: 6, ease: "easeIn", repeat: Infinity, repeatType: "reverse" }, }}
-                src={wppLogoDesktop} alt="wppLogo" height="66px" width="66px" priority className="absolute bottom-[4px] right-10 m-auto z-[100]"
-              />
+              <Image src={wppLogoDesktop} alt="wppLogo" height="66px" width="66px" priority className="wppLogo absolute bottom-[4px] right-10 m-auto z-[100]" />
             </a>
           </div>
           <div className="navMobile block fixed bottom-0 left-0 w-[100vw] sm:hidden">
             <div className="ml-6 flex absolute bottom-6 w-[40%] h-[42px] bg-primary rounded-tr-[36px] rounded-tl-[36px] rounded-br-[36px] rounded-bl-[36px] shadow-[0_0_5px_#1e8794]"></div>
             <a href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20">
-              <Image
-                initial={{ scaleX: 1, sclaeY: 1 }}
-                animate={{ scaleX: 1.1, scaleY: 1.1, transition: { duration: 2, delay: 6, ease: "easeIn", repeat: Infinity, repeatType: "reverse" }, }}
-                src={wppLogo} alt="wppLogo" height="58px" width="58px" priority className="absolute bottom-[12px] left-0 right-0 m-auto z-[100] cursor-pointer"
-              />
+              <Image src={wppLogo} alt="wppLogo" height="58px" width="58px" priority className="wppLogo absolute bottom-[12px] left-0 right-0 m-auto z-[100] cursor-pointer" />
             </a>
             <div className="mr-6 flex absolute bottom-6 right-0 w-[40%] h-[42px] bg-primary rounded-tl-[36px] rounded-tr-[36px] rounded-bl-[36px] rounded-br-[36px] shadow-[0_0_5px_#1e8794]"></div>
             <div className="flex justify-evenly items-center absolute bottom-[30px] px-0 w-[100vw] h-auto">
                 <ul>
                   <li className='flex w-[100vw] justify-evenly'>
                     <Link href={`/`} aria-label="Ir a la sección de inicio">
-                      <Image
-                        initial={{ rotate: 0 }}
-                        animate={{ rotate: [0, -10, 10, -10, 10, -10, 10, 0], transition: { duration: 2, delay: 4, ease: "easeIn", repeat: Infinity, repeatDelay: 8, repeatType: "reverse" }, }}
-                        src={navInicio} height="28px" width="28px" alt={"navegacion inicio"} priority className={`mt-[2px] w-[28px] h-[auto] text-center cursor-pointer`}
-                      />
+                      <Image src={navInicio} height="28px" width="28px" alt={"navegacion inicio"} priority className={`navLink mt-[2px] w-[28px] h-[auto] text-center cursor-pointer`} />
                     </Link>
                     <Link href={`/heladeras`} aria-label="Ir a la sección de heladeras">
-                      <Image
-                        initial={{ rotate: 0 }}
-                        animate={{ rotate: [0, -10, 10, -10, 10, -10, 10, 0], transition: { duration: 2, delay: 4, ease: "easeIn", repeat: Infinity, repeatDelay: 8, repeatType: "reverse" }, }}
-                        src={navHeladera} height="18px" width="18px" alt={"navegacion heladeras"} priority className={`w-[18px] h-[auto] mr-20 text-center cursor-pointer`}
-                      />
+                      <Image src={navHeladera} height="18px" width="18px" alt={"navegacion heladeras"} priority className={`navLink w-[18px] h-[auto] mr-20 text-center cursor-pointer`} />
                     </Link>
                     <Link href={`/lavarropas`} aria-label="Ir a la sección de lavarropas">
-                      <Image
-                        initial={{ rotate: 0 }}
-                        animate={{ rotate: [0, -10, 10, -10, 10, -10, 10, 0], transition: { duration: 2, delay: 4, ease: "easeIn", repeat: Infinity, repeatDelay: 8, repeatType: "reverse" }, }}
-                        src={navLavarropas} height="23px" width="23px" alt={"navegacion lavarropas"} priority className={`mt-[2px] w-[23px] h-[auto] text-center cursor-pointer`}
-                      />
+                      <Image src={navLavarropas} height="23px" width="23px" alt={"navegacion lavarropas"} priority className={`navLink mt-[2px] w-[23px] h-[auto] text-center cursor-pointer`} />
                     </Link>
                     <Link href={`/contacto`} aria-label="Ir a la sección de contacto">
-                      <Image
-                        initial={{ rotate: 0 }}
-                        animate={{ rotate: [0, -10, 10, -10, 10, -10, 10, 0], transition: { duration: 2, delay: 4, ease: "easeIn", repeat: Infinity, repeatDelay: 8, repeatType: "reverse" }, }}
-                        src={navContacto} height="20px" width="20px" alt={"navegacion contacto"} priority className={`mt-[3px] w-[20px] h-[auto] text-center cursor-pointer`}
-                      />
+                      <Image src={navContacto} height="20px" width="20px" alt={"navegacion contacto"} priority className={`navLink mt-[3px] w-[20px] h-[auto] text-center cursor-pointer`} />
                     </Link>
                   </li>
                 </ul>
