41306f9f6838f01e2df055ddf65425b9c436e678
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2024-07-04 00:20:28 -0300

    img wpp priority / wpp class tag manager

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -41,13 +41,15 @@ const NavBar = () => {
           </ul>
           <div className="hidden md:block fixed bottom-4 left-0 w-[100vw] ">
             <div className="blobWpp rounded-full absolute bottom-[4px] right-10 m-auto z-[100] h-[66px] w-[66px]">
-              <a href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20">
+              <a
+                className="conversion"
+                href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20"
+              >
                 <Image
                   src={wppLogoDesktop}
                   alt="wppLogo"
                   height="66px"
                   width="66px"
-                  priority
                 />
               </a>
             </div>
@@ -55,7 +57,10 @@ const NavBar = () => {
           <div className="navMobile block fixed bottom-0 left-0 w-[100vw] sm:hidden">
             <div className="ml-6 flex absolute bottom-3 w-[40%] h-[42px] bg-primary rounded-tr-[36px] rounded-tl-[36px] rounded-br-[36px] rounded-bl-[36px] shadow-[0_0_5px_#1e8794]"></div>
             <div className="blobWpp rounded-full absolute bottom-[6px] left-0 right-0 m-auto z-[100] cursor-pointer h-[60px] w-[60px]">
-              <a href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20">
+              <a
+                className="conversion"
+                href="https://api.whatsapp.com/send?phone=5491144469930&text=Hola%20👋%20service%20Electrolux%20🛠%EF%B8%8F%20tengo%20una%20consulta%3A%20"
+              >
                 <Image
                   src={wppLogo}
                   alt="wppLogo"
