12dc2b832c184919e6f808b83e3b2cba8b5a4e47
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2024-07-06 17:28:04 -0300

    fix footer 2024

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -67,12 +67,12 @@ const Footer = () => (
         </div>
 
         <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45] md:mb-6 mb-0">
-          <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-            Copyright © 2023 | Service-Electrolux
+          <p className="font-poppins font-normal text-center text-[16px] leading-[27px] text-white">
+            Copyright © 2024 | A&T electrolux
           </p>
 
           <div className="flex flex-row md:mt-0 mt-6">
-            <div className="w-[21px] h-[21px] mx-1">
+            <div className="w-[21px] h-[21px] mx-2">
               <Image
                 src={instagram}
                 alt="Social Icon"
@@ -81,7 +81,7 @@ const Footer = () => (
                 className="cursor-pointer"
               />
             </div>
-            <div className="w-[21px] h-[21px] mx-1">
+            <div className="w-[21px] h-[21px] mx-2">
               <Image
                 src={linkedin}
                 alt="Social Icon"
