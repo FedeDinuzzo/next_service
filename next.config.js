679b696786a176cec362fd9b0fb3337b2589f52b
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2026-06-23 23:58:55 -0300

    Remove www -> non-www redirect from Next config
    Delete the async redirects configuration that forced requests from www.service-electrolux.ar to https://service-electrolux.ar/:path* (permanent). Also adjust file formatting for the exported nextConfig object.

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.service-electrolux.ar",
-          },
-        ],
-        destination: "https://service-electrolux.ar/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
